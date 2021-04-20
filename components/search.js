082bc99e8cae6b288355ece0e4836748ede95069
Kunal <kunal.bohra@somaiya.edu>
2021-04-20 21:34:44 +0530

    chips done

--- components/search.js
+++ components/search.js
@@ -4,6 +4,7 @@ import styles from "../styles/Search.module.css";
 
 export default function Search() {
 	const [query, setQuery] = useState("");
+	const [choicesArray, setChoicesArray] = useState([]);
 	var input;
 
 	useEffect(() => {
@@ -35,9 +36,20 @@ export default function Search() {
 		};
 	}, []);
 
+	const handleChoices = (choice) => {
+		const tempChoices = [...choicesArray];
+		const index = tempChoices.indexOf(choice);
+		if (index < 0) {
+			tempChoices.push(choice);
+		} else {
+			tempChoices.splice(index, 1);
+		}
+		setChoicesArray(tempChoices);
+	};
+
 	return (
 		<div className="my-4">
-			<div className="w-6/12 mx-auto flex justify-items-center bg-gray-100 rounded-full">
+			<div className="sm:w-6/12 w-9/12 mx-auto flex justify-items-center bg-gray-100 rounded-full">
 				<input
 					type="text"
 					value={query}
@@ -127,7 +139,98 @@ export default function Search() {
 					<SearchRoundedIcon className="fill-current text-gray-600" />
 				</div>
 			</div>
-			
+			<div className="w-full sm:w-6/12 flex grid-flow-col gap-2 mx-auto py-4 justify-evenly">
+				<div
+					onClick={() => handleChoices("remdesivir")}
+					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-3 cursor-pointer ${
+						choicesArray.indexOf("remdesivir") > -1
+							? styles.selected
+							: ""
+					}`}
+				>
+					<span
+						className={`text-green-400  ${
+							choicesArray.indexOf("remdesivir") > -1
+								? styles.selectedText
+								: ""
+						}`}
+					>
+						Remdesivir
+					</span>
+				</div>
+				<div
+					onClick={() => handleChoices("oxygen")}
+					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
+						choicesArray.indexOf("oxygen") > -1
+							? styles.selected
+							: ""
+					}`}
+				>
+					<span
+						className={`text-green-400 ${
+							choicesArray.indexOf("oxygen") > -1
+								? styles.selectedText
+								: ""
+						}`}
+					>
+						Oxygen
+					</span>
+				</div>
+				<div
+					onClick={() => handleChoices("plasma")}
+					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
+						choicesArray.indexOf("plasma") > -1
+							? styles.selected
+							: ""
+					}`}
+				>
+					<span
+						className={`text-green-400 ${
+							choicesArray.indexOf("plasma") > -1
+								? styles.selectedText
+								: ""
+						}`}
+					>
+						Plasma
+					</span>
+				</div>
+				<div
+					onClick={() => handleChoices("tocilizumab")}
+					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
+						choicesArray.indexOf("tocilizumab") > -1
+							? styles.selected
+							: ""
+					}`}
+				>
+					<span
+						className={`text-green-400 ${
+							choicesArray.indexOf("tocilizumab") > -1
+								? styles.selectedText
+								: ""
+						}`}
+					>
+						Tocilizumab
+					</span>
+				</div>
+				<div
+					onClick={() => handleChoices("fabiflu")}
+					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
+						choicesArray.indexOf("fabiflu") > -1
+							? styles.selected
+							: ""
+					}`}
+				>
+					<span
+						className={`text-green-400 ${
+							choicesArray.indexOf("fabiflu") > -1
+								? styles.selectedText
+								: ""
+						}`}
+					>
+						Fabiflu
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 }
