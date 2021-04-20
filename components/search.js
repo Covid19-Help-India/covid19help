f389b25690b4f5e32c5a2bc2a1fc12350329fc6b
Kunal <kunal.bohra@somaiya.edu>
2021-04-20 23:00:49 +0530

    chip group added to table

--- components/search.js
+++ components/search.js
@@ -4,13 +4,15 @@ import styles from "../styles/Search.module.css";
 
 export default function Search() {
 	const [query, setQuery] = useState("");
-	const [choicesArray, setChoicesArray] = useState([]);
 	var input;
 
 	useEffect(() => {
 		input = document.querySelector("#search");
 		input.onfocus = () => {
 			cities.style.display = "block";
+		};
+		input.onblur = () => {
+			cities.style.display = "none";
 		};
 		for (let option of cities.options) {
 			option.onclick = () => {
@@ -36,16 +38,7 @@ export default function Search() {
 		};
 	}, []);
 
-	const handleChoices = (choice) => {
-		const tempChoices = [...choicesArray];
-		const index = tempChoices.indexOf(choice);
-		if (index < 0) {
-			tempChoices.push(choice);
-		} else {
-			tempChoices.splice(index, 1);
-		}
-		setChoicesArray(tempChoices);
-	};
+
 
 	return (
 		<div className="my-4">
@@ -139,98 +132,7 @@ export default function Search() {
 					<SearchRoundedIcon className="fill-current text-gray-600" />
 				</div>
 			</div>
-			<div className="w-full sm:w-6/12 flex grid-flow-col gap-2 mx-auto py-4 justify-evenly">
-				<div
-					onClick={() => handleChoices("remdesivir")}
-					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-3 cursor-pointer ${
-						choicesArray.indexOf("remdesivir") > -1
-							? styles.selected
-							: ""
-					}`}
-				>
-					<span
-						className={`text-green-400  ${
-							choicesArray.indexOf("remdesivir") > -1
-								? styles.selectedText
-								: ""
-						}`}
-					>
-						Remdesivir
-					</span>
-				</div>
-				<div
-					onClick={() => handleChoices("oxygen")}
-					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
-						choicesArray.indexOf("oxygen") > -1
-							? styles.selected
-							: ""
-					}`}
-				>
-					<span
-						className={`text-green-400 ${
-							choicesArray.indexOf("oxygen") > -1
-								? styles.selectedText
-								: ""
-						}`}
-					>
-						Oxygen
-					</span>
-				</div>
-				<div
-					onClick={() => handleChoices("plasma")}
-					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
-						choicesArray.indexOf("plasma") > -1
-							? styles.selected
-							: ""
-					}`}
-				>
-					<span
-						className={`text-green-400 ${
-							choicesArray.indexOf("plasma") > -1
-								? styles.selectedText
-								: ""
-						}`}
-					>
-						Plasma
-					</span>
-				</div>
-				<div
-					onClick={() => handleChoices("tocilizumab")}
-					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
-						choicesArray.indexOf("tocilizumab") > -1
-							? styles.selected
-							: ""
-					}`}
-				>
-					<span
-						className={`text-green-400 ${
-							choicesArray.indexOf("tocilizumab") > -1
-								? styles.selectedText
-								: ""
-						}`}
-					>
-						Tocilizumab
-					</span>
-				</div>
-				<div
-					onClick={() => handleChoices("fabiflu")}
-					className={`rounded-full border-green-400 border-2 bg-gradient-to-r py-1 px-2 cursor-pointer ${
-						choicesArray.indexOf("fabiflu") > -1
-							? styles.selected
-							: ""
-					}`}
-				>
-					<span
-						className={`text-green-400 ${
-							choicesArray.indexOf("fabiflu") > -1
-								? styles.selectedText
-								: ""
-						}`}
-					>
-						Fabiflu
-					</span>
-				</div>
-			</div>
+		
 		</div>
 	);
 }
--- pages/table.js
+++ pages/table.js
@@ -1,7 +1,6 @@
 import { forwardRef, useState, useEffect } from "react";
 
-import ReactDOM from "react-dom";
-import MaterialTable from "material-table";
+import MaterialTable, { MTableToolbar } from "material-table";
 
 import axios from "axios";
 
@@ -20,61 +19,98 @@ import Remove from "@material-ui/icons/Remove";
 import SaveAlt from "@material-ui/icons/SaveAlt";
 import Search from "@material-ui/icons/Search";
 import ViewColumn from "@material-ui/icons/ViewColumn";
+import ChipGroup from "../components/chipGroup";
 
 export default function Table() {
-    const api = axios.create({
-        baseURL: `https://reqres.in/api`,
-    });
+	const api = axios.create({
+		baseURL: `https://reqres.in/api`,
+	});
 
-    const [data, setData] = useState([]); //table data
-    //for error handling
+	const [data, setData] = useState([]); //table data
+	//for error handling
 
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-    };
+	const tableIcons = {
+		Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+		Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+		Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+		Delete: forwardRef((props, ref) => (
+			<DeleteOutline {...props} ref={ref} />
+		)),
+		DetailPanel: forwardRef((props, ref) => (
+			<ChevronRight {...props} ref={ref} />
+		)),
+		Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+		Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+		Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+		FirstPage: forwardRef((props, ref) => (
+			<FirstPage {...props} ref={ref} />
+		)),
+		LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+		NextPage: forwardRef((props, ref) => (
+			<ChevronRight {...props} ref={ref} />
+		)),
+		PreviousPage: forwardRef((props, ref) => (
+			<ChevronLeft {...props} ref={ref} />
+		)),
+		ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+		Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+		SortArrow: forwardRef((props, ref) => (
+			<ArrowDownward {...props} ref={ref} />
+		)),
+		ThirdStateCheck: forwardRef((props, ref) => (
+			<Remove {...props} ref={ref} />
+		)),
+		ViewColumn: forwardRef((props, ref) => (
+			<ViewColumn {...props} ref={ref} />
+		)),
+	};
 
-    const columns = [
-        { title: "id", field: "id", hidden: true },
-        { title: "First name", field: "first_name" },
-        { title: "Last name", field: "last_name" },
-        { title: "email", field: "email" },
-    ];
+	const columns = [
+		{ title: "id", field: "id", hidden: true },
+		{ title: "First name", field: "first_name" },
+		{ title: "Last name", field: "last_name" },
+		{
+			title: "email",
+			field: "email",
+		},
+	];
 
-    const options = {
-        pageSize: 10,
-        pageSizeOptions: [5, 10, 25, 50, 100],
-    };
+	const options = {
+		pageSize: 10,
+		pageSizeOptions: [5, 10, 25, 50, 100],
+		filtering: true,
+	};
 
-    useEffect(() => {
-        api.get("/users")
-            .then((res) => {
-                setData(res.data.data);
-            })
-            .catch((error) => {
-                setErrorMessage(["Cannot load user data"]);
-                setIserror(true);
-            });
-    }, []);
+	const components = {
+		Toolbar: (props) => (
+			<div>
+				<MTableToolbar {...props} />
+				<ChipGroup />
+			</div>
+		),
+	};
 
-    return (
-        <div style={{ maxWidth: "100%" }}>
-            <MaterialTable columns={columns} data={data} options={options} title="Covid19 Help India" icons={tableIcons} />
-        </div>
-    );
+	useEffect(() => {
+		api.get("/users")
+			.then((res) => {
+				setData(res.data.data);
+			})
+			.catch((error) => {
+				setErrorMessage(["Cannot load user data"]);
+				setIserror(true);
+			});
+	}, []);
+
+	return (
+		<div style={{ maxWidth: "100%" }}>
+			<MaterialTable
+				columns={columns}
+				data={data}
+				components={components}
+				options={options}
+				title="Covid19 Help India"
+				icons={tableIcons}
+			/>
+		</div>
+	);
 }
