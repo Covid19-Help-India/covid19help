83734e36229e547556b93c1a4d3d3238dbd70bdf
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-04-20 15:59:42 +0530

    Editable table

--- components/table.js
+++ components/table.js
@@ -84,9 +84,40 @@ export default function Table() {
         },
     ];
 
+    const editable = {
+        onRowAdd: (newData) =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    setData([...data, newData]);
+
+                    resolve();
+                }, 1000);
+            }),
+        onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    const dataUpdate = [...data];
+                    const index = oldData.tableData.id;
+                    dataUpdate[index] = newData;
+                    setData([...dataUpdate]);
+                    resolve();
+                }, 1000);
+            }),
+        onRowDelete: (oldData) =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    const dataDelete = [...data];
+                    const index = oldData.tableData.id;
+                    dataDelete.splice(index, 1);
+                    setData([...dataDelete]);
+                    resolve();
+                }, 1000);
+            }),
+    };
+
     return (
         <div style={{ maxWidth: "100%" }}>
-            <MaterialTable columns={columns} data={data} title="Demo Title" />
+            <MaterialTable columns={columns} data={data} title="Demo Title" editable={editable} />
         </div>
     );
 }
