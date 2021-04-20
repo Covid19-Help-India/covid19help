e323a905e917ff4edb95a7cb57dd765353fe6e30
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-04-20 16:29:06 +0530

    Navbar updated

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 export default function Nav() {
+    const style = {
+        height: "60px",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: "10px",
+    };
     return (
-        <div style={{ height: "60px" }}>
-            <p>Navbar</p>
+        <div style={style}>
+            <Image src="/logo.png" alt="Picture of the author" width={50} height={50} />
+            <div style={{ marginRight: "10px", marginLeft: "10px", fontSize: "24px" }}>Covid19 Help India</div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ export default function Home() {
                 <title>Create Next App</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
             </Head>
 
             <Nav />
