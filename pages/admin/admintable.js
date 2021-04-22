046fab44a5a24e2d3c6c45ba8f16cb9af3bc7ac1
Kunal <kunal.bohra@somaiya.edu>
2021-04-22 20:00:07 +0530

    APi requests for add, update, delete

--- pages/admin/admintable.js
+++ pages/admin/admintable.js
@@ -25,220 +25,312 @@ import Search from "@material-ui/icons/Search";
 import ViewColumn from "@material-ui/icons/ViewColumn";
 
 const useStyles = makeStyles({
-    toolbarWrapper: {
-        "& .MuiToolbar-gutters": {
-            paddingLeft: 20,
-            paddingRight: 20,
-        },
-        "& .MTableToolbar-searchField-11": {
-            minWidth: "90%",
-            paddingLeft: 0,
-            paddingRight: 0,
-        },
-        "& .MuiTablePagination-caption": {
-            display: "none",
-        },
-        "& th": {
-            width: "200px",
-        },
-    },
+	toolbarWrapper: {
+		"& .MuiToolbar-gutters": {
+			paddingLeft: 20,
+			paddingRight: 20,
+		},
+		"& .MTableToolbar-searchField-11": {
+			minWidth: "90%",
+			paddingLeft: 0,
+			paddingRight: 0,
+		},
+		"& .MuiTablePagination-caption": {
+			display: "none",
+		},
+		"& th": {
+			width: "200px",
+		},
+	},
 });
 
 export default function AdminTable() {
-    const classes = useStyles();
+	const classes = useStyles();
+	const api = axios.create({
+		baseURL: publicRuntimeConfig.BACKEND_URL,
+	});
 
-    const [data, setData] = useState([]); //table data
-    const [errorMessage, setErrorMessage] = useState(""); //error message
-    const [error, setError] = useState(false); //error
-    const [windowHeight, setWindowHeight] = useState("");
+	const [data, setData] = useState([]); //table data
+	const [errorMessage, setErrorMessage] = useState(""); //error message
+	const [error, setError] = useState(false); //error
+	const [windowHeight, setWindowHeight] = useState("");
 
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-    };
+	const tableIcons = {
+		Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+		Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+		Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+		Delete: forwardRef((props, ref) => (
+			<DeleteOutline {...props} ref={ref} />
+		)),
+		DetailPanel: forwardRef((props, ref) => (
+			<ChevronRight {...props} ref={ref} />
+		)),
+		Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+		Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+		Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+		FirstPage: forwardRef((props, ref) => (
+			<FirstPage {...props} ref={ref} />
+		)),
+		LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+		NextPage: forwardRef((props, ref) => (
+			<ChevronRight {...props} ref={ref} />
+		)),
+		PreviousPage: forwardRef((props, ref) => (
+			<ChevronLeft {...props} ref={ref} />
+		)),
+		ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+		Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+		SortArrow: forwardRef((props, ref) => (
+			<ArrowDownward {...props} ref={ref} />
+		)),
+		ThirdStateCheck: forwardRef((props, ref) => (
+			<Remove {...props} ref={ref} />
+		)),
+		ViewColumn: forwardRef((props, ref) => (
+			<ViewColumn {...props} ref={ref} />
+		)),
+	};
 
-    const columns = [
-        { title: "id", field: "id", hidden: true },
-        { title: "State", field: "State", cellStyle: { textAlign: "center", border: "0.5px solid lightgray" } },
-        { title: "City", field: "City", cellStyle: { textAlign: "center", border: "0.5px solid lightgray" } },
-        { title: "Category", field: "Category" },
-        { title: "Distributor Name", field: "Distributor" },
-        { title: "Distributor Contact", field: "DistPhNo" },
-        { title: "Distributor Address", field: "DistAddress", headerStyle: { width: "10px" } },
-        { title: "Pincode", field: "Pincode", cellStyle: { textAlign: "center", border: "0.5px solid lightgray" } },
-        { title: "Upvotes", field: "Upvotes", cellStyle: { textAlign: "center", border: "0.5px solid lightgray" } },
-        { title: "Downvotes", field: "Downvotes", cellStyle: { textAlign: "center", border: "0.5px solid lightgray" } },
-        { title: "Source", field: "Source" },
-    ];
+	const columns = [
+		{ title: "id", field: "id", hidden: true },
+		{
+			title: "State",
+			field: "State",
+			cellStyle: { textAlign: "center", border: "0.5px solid lightgray" },
+		},
+		{
+			title: "City",
+			field: "City",
+			cellStyle: { textAlign: "center", border: "0.5px solid lightgray" },
+		},
+		{ title: "Category", field: "Category" },
+		{ title: "Distributor Name", field: "Distributor" },
+		{ title: "Distributor Contact", field: "DistPhNo" },
+		{
+			title: "Distributor Address",
+			field: "DistAddress",
+			headerStyle: { width: "10px" },
+		},
+		{
+			title: "Pincode",
+			field: "Pincode",
+			cellStyle: { textAlign: "center", border: "0.5px solid lightgray" },
+		},
+		{
+			title: "Upvotes",
+			field: "Upvotes",
+			cellStyle: { textAlign: "center", border: "0.5px solid lightgray" },
+		},
+		{
+			title: "Downvotes",
+			field: "Downvotes",
+			cellStyle: { textAlign: "center", border: "0.5px solid lightgray" },
+		},
+		{ title: "Source", field: "Source" },
+	];
 
-    const options = {
-        pageSize: 10,
-        pageSizeOptions: [10, 25, 50, 100],
-        showTitle: false,
-        maxBodyHeight: windowHeight - 0.015 * windowHeight,
-        headerStyle: {
-            border: "0.5px solid lightgray",
-            background: "#1da1f2",
-            color: "#f5f5f5",
-            textAlign: "left",
-            whiteSpace: "nowrap",
-        },
-        cellStyle: {
-            border: "0.5px solid lightgray",
-            whiteSpace: "nowrap",
-        },
-        paginationType: "stepped",
-        addRowPosition: "first",
-    };
+	const options = {
+		pageSize: 10,
+		pageSizeOptions: [10, 25, 50, 100],
+		showTitle: false,
+		maxBodyHeight: windowHeight - 0.015 * windowHeight,
+		headerStyle: {
+			border: "0.5px solid lightgray",
+			background: "#1da1f2",
+			color: "#f5f5f5",
+			textAlign: "left",
+			whiteSpace: "nowrap",
+		},
+		cellStyle: {
+			border: "0.5px solid lightgray",
+			whiteSpace: "nowrap",
+		},
+		paginationType: "stepped",
+		addRowPosition: "first",
+	};
 
-    const localization = {
-        pagination: {
-            labelDisplayedRows: "",
-            labelRowsSelect: "",
-            labelRowsPerPage: "",
-            firstAriaLabel: "First Page",
-            firstTooltip: "First Page",
-            previousAriaLabel: "Previous Page",
-            previousTooltip: "Previous Page",
-            nextAriaLabel: "Next Page",
-            nextTooltip: "Next Page",
-            lastAriaLabel: "Last Page",
-            lastTooltip: "Last Page",
-        },
-    };
+	const localization = {
+		pagination: {
+			labelDisplayedRows: "",
+			labelRowsSelect: "",
+			labelRowsPerPage: "",
+			firstAriaLabel: "First Page",
+			firstTooltip: "First Page",
+			previousAriaLabel: "Previous Page",
+			previousTooltip: "Previous Page",
+			nextAriaLabel: "Next Page",
+			nextTooltip: "Next Page",
+			lastAriaLabel: "Last Page",
+			lastTooltip: "Last Page",
+		},
+	};
 
-    const components = {
-        Toolbar: (props) => (
-            <div className={classes.toolbarWrapper}>
-                <MTableToolbar {...props} />
-            </div>
-        ),
-    };
+	const components = {
+		Toolbar: (props) => (
+			<div className={classes.toolbarWrapper}>
+				<MTableToolbar {...props} />
+			</div>
+		),
+	};
 
-    const handleRowAdd = (newData, resolve) => {
-        let errorList = [];
-        if (newData.first_name === undefined) {
-            errorList.push("Please enter first name");
-        }
-        if (newData.last_name === undefined) {
-            errorList.push("Please enter last name");
-        }
-        if (newData.email === undefined) {
-            errorList.push("Please enter a valid email");
-        }
-        if (errorList.length < 1) {
-            api.post("/users", newData)
-                .then((res) => {
-                    let dataToAdd = [...data];
-                    dataToAdd.push(newData);
-                    setData(dataToAdd);
-                    resolve();
-                    setErrorMessages([]);
-                    setIserror(false);
-                })
-                .catch((error) => {
-                    setErrorMessages(["Cannot add data. Server error!"]);
-                    setIserror(true);
-                    resolve();
-                });
-        } else {
-            setErrorMessages(errorList);
-            setIserror(true);
-            resolve();
-        }
-    };
+	const handleRowAdd = (newData, resolve) => {
+		let errorList = [];
+		console.log(newData);
+		if (newData.City === undefined) {
+			errorList.push("Please enter city");
+		}
+		if (newData.Category === undefined) {
+			errorList.push("Please enter category of resource");
+		}
+		if (newData.Distributor === undefined) {
+			errorList.push("Please enter distributor name");
+		}
+		if (newData.DistPhNo === undefined) {
+			errorList.push("Please enter distributor contact info");
+		}
 
-    const handleRowUpdate = (newData, oldData, resolve) => {
-        //validation
-        if (errorList.length < 1) {
-            api.patch("/users/" + newData.id, newData)
-                .then((res) => {
-                    const dataUpdate = [...data];
-                    const index = oldData.tableData.id;
-                    dataUpdate[index] = newData;
-                    setData([...dataUpdate]);
-                    resolve();
-                    setIserror(false);
-                    setErrorMessages([]);
-                })
-                .catch((error) => {
-                    setErrorMessages(["Update failed! Server error"]);
-                    setIserror(true);
-                    resolve();
-                });
-        } else {
-            setErrorMessages(errorList);
-            setIserror(true);
-            resolve();
-        }
-    };
+		if (errorList.length < 1) {
+			api.post("/add_info", newData)
+				.then((res) => {
+					if (res.status) {
+						let dataToAdd = [...data];
+						newData[id] = res.id;
+						dataToAdd.push(newData);
+						setData(dataToAdd);
+						setErrorMessages([]);
+						setIserror(false);
+					} else {
+						setErrorMessages(["Cannot add data!"]);
+						setIserror(true);
+						console.log(errorMessage);
+					}
+					resolve();
+				})
+				.catch((error) => {
+					setErrorMessages(["Cannot add data! " + error]);
+					setIserror(true);
+					console.log(errorMessage);
+					resolve();
+				});
+		} else {
+			setErrorMessages(errorList);
+			setIserror(true);
+			resolve();
+		}
+	};
 
-    const handleRowDelete = (oldData, resolve) => {
-        api.delete("/users/" + oldData.id)
-            .then((res) => {
-                const dataDelete = [...data];
-                const index = oldData.tableData.id;
-                dataDelete.splice(index, 1);
-                setData([...dataDelete]);
-                resolve();
-            })
-            .catch((error) => {
-                setErrorMessages(["Delete failed! Server error"]);
-                setIserror(true);
-                resolve();
-            });
-    };
+	const handleRowUpdate = (newData, oldData, resolve) => {
+		//validation
+		let errorList = [];
+		console.log(newData);
+		if (newData.City === undefined) {
+			errorList.push("Please enter city");
+		}
+		if (newData.Category === undefined) {
+			errorList.push("Please enter category of resource");
+		}
+		if (newData.Distributor === undefined) {
+			errorList.push("Please enter distributor name");
+		}
+		if (newData.DistPhNo === undefined) {
+			errorList.push("Please enter distributor contact info");
+		}
 
-    const editable = {
-        onRowUpdate: (newData, oldData) =>
-            new Promise((resolve) => {
-                handleRowUpdate(newData, oldData, resolve);
-            }),
-        onRowAdd: (newData) =>
-            new Promise((resolve) => {
-                handleRowAdd(newData, resolve);
-            }),
-        onRowDelete: (oldData) =>
-            new Promise((resolve) => {
-                handleRowDelete(oldData, resolve);
-            }),
-    };
+		if (errorList.length < 1) {
+			api.post("/edit_info", newData)
+				.then((res) => {
+					if (res.status) {
+						const dataUpdate = [...data];
+						const index = oldData.tableData.id;
+						dataUpdate[index] = newData;
+						setData([...dataUpdate]);
+						setIserror(false);
+						setErrorMessages([]);
+					} else {
+						setErrorMessages(["Update failed! " + error]);
+						setIserror(true);
+					}
+					resolve();
+				})
+				.catch((error) => {
+					setErrorMessages(["Update failed! " + error]);
+					setIserror(true);
+					console.log(errorMessage);
+					resolve();
+				});
+		} else {
+			setErrorMessages(errorList);
+			setIserror(true);
+			resolve();
+		}
+	};
 
-    useEffect(() => {
-        setWindowHeight(window.innerHeight - 217);
-        const api = axios.create({
-            baseURL: publicRuntimeConfig.BACKEND_URL,
-        });
-        let formData = new FormData();
-        formData.append("City", "Mumbai");
-        api.post("/get_info", formData)
-            .then((res) => {
-                setData(res.data);
-            })
-            .catch((error) => {
-                setErrorMessage(["Cannot load user data"]);
-                setError(true);
-            });
-    }, []);
+	const handleRowDelete = (oldData, resolve) => {
+		let formData = new FormData();
+		formData.append("entry_id", oldData["id"]);
+		api.post("/del_info", formData)
+			.then((res) => {
+				if (res.status) {
+					const dataDelete = [...data];
+					const index = oldData.tableData.id;
+					dataDelete.splice(index, 1);
+					setData([...dataDelete]);
+				} else {
+					setErrorMessages(["Delete failed!"]);
+					setIserror(true);
+					console.log(errorMessage);
+				}
+				resolve();
+			})
+			.catch((error) => {
+				setErrorMessages(["Delete failed! " + error]);
+				setIserror(true);
+				resolve();
+			});
+	};
 
-    return (
-        <div style={{ maxWidth: "100%" }}>
-            <MaterialTable columns={columns} components={components} localization={localization} data={data} options={options} title="Covid19 Help India" icons={tableIcons} editable={editable} />
-        </div>
-    );
+	const editable = {
+		onRowUpdate: (newData, oldData) =>
+			new Promise((resolve) => {
+				handleRowUpdate(newData, oldData, resolve);
+			}),
+		onRowAdd: (newData) =>
+			new Promise((resolve) => {
+				handleRowAdd(newData, resolve);
+			}),
+		onRowDelete: (oldData) =>
+			new Promise((resolve) => {
+				handleRowDelete(oldData, resolve);
+			}),
+	};
+
+	useEffect(() => {
+		setWindowHeight(window.innerHeight - 217);
+
+		let formData = new FormData();
+		formData.append("City", "Mumbai");
+		api.post("/get_info", formData)
+			.then((res) => {
+				setData(res.data);
+			})
+			.catch((error) => {
+				setErrorMessage(["Cannot load user data " + error]);
+				setError(true);
+			});
+	}, []);
+
+	return (
+		<div style={{ maxWidth: "100%" }}>
+			<MaterialTable
+				columns={columns}
+				components={components}
+				localization={localization}
+				data={data}
+				options={options}
+				title="Covid19 Help India"
+				icons={tableIcons}
+				editable={editable}
+			/>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Nav from "../components/nav.js";
 import Footer from "../components/footer.js";
-import Search from "../components/search.js";
 import Table from "./table.js";
 import Head from "next/head";
 import { useState, useEffect } from "react";
@@ -8,22 +7,25 @@ import { resetServerContext } from "react-beautiful-dnd";
 import "../styles/Table.module.css";
 
 export default function Home() {
-    resetServerContext();
-    const [windowHeight, setWindowHeight] = useState("");
+	resetServerContext();
+	const [windowHeight, setWindowHeight] = useState("");
 
-    useEffect(() => {
-        setWindowHeight(window.innerHeight - 56);
-    }, []);
+	useEffect(() => {
+		setWindowHeight(window.innerHeight - 56);
+	}, []);
 
-    return (
-        <div>
-            <Head>
-                <title>Covid19 Help India</title>
-                <link rel="icon" href="/favicon.ico" />
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195041793-1" />
-                <script
-                    dangerouslySetInnerHTML={{
-                                        __html: `
+	return (
+		<div>
+			<Head>
+				<title>Covid19 Help India</title>
+				<link rel="icon" href="/favicon.ico" />
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=UA-195041793-1"
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
                         window.dataLayer = window.dataLayer || []
                         function gtag(){
                             dataLayer.push(arguments)
@@ -31,27 +33,32 @@ export default function Home() {
                         gtag('js', new Date())
                         gtag('config', 'UA-195041793-1')
                         `,
-                    }}
-                />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
-            </Head>
+					}}
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/icon?family=Material+Icons"
+				/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 
-            <Nav />
-            <Search />
-            <div
-                style={{
-                    padding: "10px",
-                    maxWidth: "1200px",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                }}
-            >
-                <Table />
-            </div>
+			<Nav />
+			<div
+				style={{
+					padding: "10px",
+					maxWidth: "1200px",
+					marginLeft: "auto",
+					marginRight: "auto",
+				}}
+			>
+				<Table />
+			</div>
 
-            {/* <Footer /> */}
-        </div>
-    );
+			{/* <Footer /> */}
+		</div>
+	);
 }
