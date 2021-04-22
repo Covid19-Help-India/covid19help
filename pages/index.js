6ac568b870a0a4841c0afae7b72ee5651e69b987
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-04-22 13:29:12 +0530

    Added Google Analytics Code

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,19 @@ export default function Home() {
             <Head>
                 <title>Covid19 Help India</title>
                 <link rel="icon" href="/favicon.ico" />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195041793-1" />
+                <script
+                    dangerouslySetInnerHTML={{
+                                        __html: `
+                        window.dataLayer = window.dataLayer || []
+                        function gtag(){
+                            dataLayer.push(arguments)
+                        }
+                        gtag('js', new Date())
+                        gtag('config', 'UA-195041793-1')
+                        `,
+                    }}
+                />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
