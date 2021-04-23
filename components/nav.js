5385d349d45450ef5ccda8fde00cba68404b8b5d
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-04-23 22:07:59 +0530

    Navbar design updated

--- components/nav.js
+++ components/nav.js
@@ -10,7 +10,7 @@ export default function Nav() {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        padding: "10px",
+        padding: "10px 8px",
         boxShadow: "0px 0px 12px -4px rgba(0,0,0,1)",
     };
 
@@ -25,27 +25,27 @@ export default function Nav() {
 
     return (
         <div id="navbar" style={style}>
-            <a href="/">
-                <Image className="branding-logo" src="/logo.png" alt="Picture of the author" width={36} height={36} />
+            <a href="/" style={{ width: "32px", height: "32px" }}>
+                <Image className="branding-logo" src="/logo.png" alt="Picture of the author" width={32} height={32} />
             </a>
-            <div style={{ marginRight: "auto", marginLeft: "10px", fontSize: "20px" }}>Covid19 Help India</div>
-            <div style={{ marginRight: "8px", maxHeight: "28px" }}>
-                <a target="_blank" href="https://www.twitter.com/covid19help_in">
+            <div style={{ marginRight: "auto", marginLeft: "6px", fontSize: "20px" }}>Covid19 Help India</div>
+            <div style={{ marginRight: "6px", maxHeight: "28px" }}>
+                <a target="_blank" href="https://www.twitter.com/covid19help_in" style={{ width: "28px", height: "28px" }}>
                     <Image className="social-links" src="/twitter.png" alt="Picture of the author" width={28} height={28} />
                 </a>
             </div>
-            <div style={{ marginRight: "8px", maxHeight: "28px" }}>
-                <a target="_blank" href="https://www.instagram.com/covid19help.in" style={{ marginLeft: "10px" }}>
+            <div style={{ marginRight: "6px", maxHeight: "28px" }}>
+                <a target="_blank" href="https://www.instagram.com/covid19help.in" style={{ marginLeft: "10px", width: "28px", height: "28px" }}>
                     <Image className="social-links" src="/instagram.png" alt="Picture of the author" width={28} height={28} />
                 </a>
             </div>
             <div style={{ marginRight: "2px", maxHeight: "28px" }}>
                 {!isLoggedIn || window.location.pathname == "/" ? (
-                    <a style={{ marginLeft: "10px" }} href="/admin">
+                    <a href="/admin" style={{ marginLeft: "10px", width: "28px", height: "28px" }}>
                         <Image className="navbar-account" src="/account.png" alt="Picture of the author" width={28} height={28} />
                     </a>
                 ) : (
-                    <button className="navbar-account" style={{ marginLeft: "10px" }} onClick={handleLogout}>
+                    <button className="navbar-account" style={{ marginLeft: "10px", width: "28px", height: "28px" }} onClick={handleLogout}>
                         <Image src="/logout2.png" alt="Picture of the author" width={28} height={28} />
                     </button>
                 )}
