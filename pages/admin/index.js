a109ae91c34b2f5418a477c3af98d8fc187ce88e
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-04-26 17:14:12 +0530

    Update

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,6 +2,7 @@ import Nav from "../../components/nav.js";
 import Footer from "../../components/footer.js";
 import AdminTable from "./admintable.js";
 import Login from "./Login.js";
+import Head from "next/head";
 import { useState, useEffect } from "react";
 
 export default function Admin() {
@@ -16,11 +17,58 @@ export default function Admin() {
     if (isLoggedIn) {
         return (
             <div>
+                <Head>
+                    <title>Covid19 Help India Admin</title>
+                    <link rel="icon" href="../covid19icon.ico" />
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195041793-1" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        window.dataLayer = window.dataLayer || []
+                        function gtag(){
+                            dataLayer.push(arguments)
+                        }
+                        gtag('js', new Date())
+                        gtag('config', 'UA-195041793-1')
+                        `,
+                        }}
+                    />
+
+                    <meta name="keywords" content="Covid19 Help India" />
+                    <meta name="language" content="EN" />
+                    <meta name="robots" content="INDEX, FOLLOW" />
+                    <meta name="author" content="Covid19 Help India" />
+                    <meta name="designer" content="Covid19 Help India" />
+                    <meta name="copyright" content="Covid19 Help India" />
+                    <meta name="owner" content="Covid19 Help India" />
+                    <meta name="geo.region" content="IN" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+                    <meta name="og:locality" content="Mumbai" />
+                    <meta name="og:region" content="IN" />
+                    <meta name="og:title" content="Covid19 Help India" />
+
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content="Covid19 Help India" />
+                    <meta property="og:title" content="Covid19 Help India" />
+                    <meta property="og:description" content="Covid19 Help India" />
+                    <meta property="og:image" content="/logo.png" />
+                    <meta property="og:url" content="https://covid19help-in.herokuapp.com/" />
+
+                    <meta property="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:site" content="https://www.twitter.com/covid19help_in/" />
+                    <meta property="twitter:title" content="Covid19 Help India" />
+                    <meta property="twitter:description" content="Covid19 Help India" />
+                    <meta property="twitter:url" content="https://www.twitter.com/covid19help_in/" />
+
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+                </Head>
                 <Nav />
 
                 <div
                     style={{
-                        // padding: "1rem 10px",
                         maxWidth: "100%",
                         marginLeft: "auto",
                         marginRight: "auto",
@@ -35,6 +83,54 @@ export default function Admin() {
     } else {
         return (
             <div>
+                <Head>
+                    <title>Covid19 Help India Admin</title>
+                    <link rel="icon" href="../covid19icon.ico" />
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195041793-1" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        window.dataLayer = window.dataLayer || []
+                        function gtag(){
+                            dataLayer.push(arguments)
+                        }
+                        gtag('js', new Date())
+                        gtag('config', 'UA-195041793-1')
+                        `,
+                        }}
+                    />
+
+                    <meta name="keywords" content="Covid19 Help India" />
+                    <meta name="language" content="EN" />
+                    <meta name="robots" content="INDEX, FOLLOW" />
+                    <meta name="author" content="Covid19 Help India" />
+                    <meta name="designer" content="Covid19 Help India" />
+                    <meta name="copyright" content="Covid19 Help India" />
+                    <meta name="owner" content="Covid19 Help India" />
+                    <meta name="geo.region" content="IN" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+                    <meta name="og:locality" content="Mumbai" />
+                    <meta name="og:region" content="IN" />
+                    <meta name="og:title" content="Covid19 Help India" />
+
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content="Covid19 Help India" />
+                    <meta property="og:title" content="Covid19 Help India" />
+                    <meta property="og:description" content="Covid19 Help India" />
+                    <meta property="og:image" content="/logo.png" />
+                    <meta property="og:url" content="https://covid19help-in.herokuapp.com/" />
+
+                    <meta property="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:site" content="https://www.twitter.com/covid19help_in/" />
+                    <meta property="twitter:title" content="Covid19 Help India" />
+                    <meta property="twitter:description" content="Covid19 Help India" />
+                    <meta property="twitter:url" content="https://www.twitter.com/covid19help_in/" />
+
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+                </Head>
                 <Nav />
 
                 <div
